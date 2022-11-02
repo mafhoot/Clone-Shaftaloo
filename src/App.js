9ad62f9bb50c3991a92ceb148776d8f13bab6c4c
hossein <ah.rahimi19219@gmail.com>
2022-11-02 22:05:08 +0330

    base project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+    <div>
+      <header>
+        <h1>blank page</h1>
+      </header>
     </div>
   );
 }
--- src/services/axios.js
+++
@@ -1,6 +0,0 @@
-import { url } from "../consts";
-import axios from "axios";
-
-export function postUser(data) {
-    return axios.post(url+"api/User",data)
-}
