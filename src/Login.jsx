6b54e102146f72d3cebfa734a1ef031904f8bb44
Mafhoot <Mafhoot@gmail.com>
2022-11-03 22:22:43 +0330

    Lot of changes :D

--- src/Login.jsx
+++ src/Login.jsx
@@ -31,13 +31,13 @@ export const Login = (props) => {
             <h1>LOGIN</h1>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">E-Mail</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Example@gmail.com" id="email" name="email" />
+                <input value={email} required onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Example@gmail.com" id="email" name="email" />
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={pass} required onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <button type="submit" >Log In</button>
             </form>
             <p>Don't have an account? <a className="link-btn" onClick={() => props.onFormSwitch('register')}>Register Now</a></p>
-            <span>{loginFlag}</span>
+            <span className="loginError">{loginFlag}</span>
         </div>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect,useRef } from "react";
 import { postUser } from "./services/axios";
 
 export const Register = (props) => {
@@ -7,37 +7,69 @@ export const Register = (props) => {
     const [name, setName] = useState('');
     const [pass2, setPass2] = useState('');
     const [emailcheck ,setEmailcheck] = useState("")
+    const [passcheck,setPasscheck] = useState ("")
+    const [pass2check,setPass2check] = useState ("")
+    var flag=0
+    
+    const adminuser = { 
+        email : "admin@admin.com"
+    }
+
+    const resetpage = () =>{
+        setEmailcheck("")
+        setPasscheck("")
+        setPass2check("")
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
-        //if (pass==pass2){
-            
-        //}
-        postUser({
+        if (email==adminuser.email) {
+            setEmailcheck ("This email is alredy exist")
+            flag=1
+        }
+        if (pass!=pass2){
+            setPass2check("Passwords don't match")
+            flag=1
+        }
+        if (pass.length<6){
+            setPasscheck("Password must be more than 6 letter")
+            flag=1
+        }
+
+        /*postUser({
             "username":name,
             "password": pass,
             "email": email
           } ).then(x=>{
             console.log(x);
-          }) ;
-        	
+          }) ;*/
 
+        
+        if (flag==0){
+            alert ("Register completed")
+        }
 
-      }
+    }
+
+    useEffect (resetpage, [passcheck,pass2check,emailcheck])
+
 
     return (
         <div className="auth-form-container">
             <h1>REGISTER</h1>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="Your full name" />
+            <input value={name} required onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="Your full name" />
             <label htmlFor="email">E-Mail</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Example@gmail.com" id="email" name="email" />
+            <input value={email} required onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Example@gmail.com" id="email" name="email" />
+            <span className="registerError">{emailcheck}</span>
             <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <input value={pass} required onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <span className="registerError">{passcheck}</span>
             <label htmlFor="password2">Re-type password</label>
-            <input value={pass2} onChange={(e) => setPass2(e.target.value)} type="password" placeholder="********" id="password2" name="password2" />
+            <input value={pass2} required onChange={(e) => setPass2(e.target.value)} type="password" placeholder="********" id="password2" name="password2" />
+            <span className="registerError">{pass2check}</span>
             
             <button type="submit">Register</button>
         </form>
