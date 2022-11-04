cc8f2f99e194f2cdfd826076de00512707376a84
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2022-11-04 16:04:04 +0330

    'add'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import "./App.css";
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./Components/Auth/Login/Login";
+import { Register } from "./Components/Auth/SignUp/Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <div>
-      <header>
-        <h1>blank page</h1>
-      </header>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
-/////.....
-export default App;
-///////
-/////
 
-//////////////////////////////
+export default App;
