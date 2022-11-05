20855b800518ddc3ef529e4d831508af3c5218de
Mafhoot <Mafhoot@gmail.com>
2022-11-05 22:30:22 +0330

    123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { Restaurant_page } from './restaurant_page'
+import './restaurant_page.css'
 
 function App() {
   return (
--- src/restaurant_page.jsx
+++ src/restaurant_page.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import pic from "./Images/store-banner.jpg"
+//import pic from "./Images/store-banner.jpg"
 
 export const Restaurant_page = () => {
     return (
-        
-      <div className="image"></div>
-        
+      <div>
+
+      <div className="HeadImage"></div>
+      <div className="logo"></div>
+
+      </div>
     )
 }
