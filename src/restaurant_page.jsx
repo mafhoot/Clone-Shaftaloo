1dd6d8543848451adffc46d11a090a371b98218e
Mafhoot <Mafhoot@gmail.com>
2022-11-06 15:46:31 +0330

    Working ...

--- src/restaurant_page.jsx
+++ src/restaurant_page.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 //import pic from "./Images/store-banner.jpg"
 
 export const Restaurant_page = () => {
-    return (
-      <div>
+    
 
-      <div className="HeadImage"></div>
-      <div className="logo"></div>
-      <div className="info" >
-        <label className="name">My Name</label>
-        <b />
-        <p className="description" >The first and best brand of fried food in the world</p>
-      </div>
-
-      </div>
-    )
+      return (
+        <div className="All">
+        
+        <div className="HeadImage"></div>
+        <div className="logo"></div>
+        <div className="details">
+        <div className="info" >
+            <label className="name">KFC</label>
+            <p className="description" >The first and best brand of fried foods in the world</p>
+            <p className="tags">Tags :</p>
+          </div>
+        </div>
+        </div>
+      )
 }
