1a31f5b5b0202dd96883333e44383ced495c899b
Mafhoot <Mafhoot@gmail.com>
2022-11-07 23:10:41 +0330

    working

--- src/restaurant_page.jsx
+++ src/restaurant_page.jsx
@@ -1,10 +1,12 @@
 import React,{useState} from "react";
+import { Rate } from 'antd';
 //import pic from "./Images/store-banner.jpg"
 
 export const Restaurant_page = () => {
     
 
       return (
+        <>
         <div className="All">
         
         <div className="HeadImage"></div>
@@ -13,9 +15,12 @@ export const Restaurant_page = () => {
         <div className="info" >
             <label className="name">KFC</label>
             <p className="description" >The first and best brand of fried foods in the world</p>
-            <p className="tags">Tags :</p>
+            <p className="tags">Tagssss :</p>
+            <p></p>
           </div>
         </div>
         </div>
+        <Rate  />
+        </>
       )
 }
