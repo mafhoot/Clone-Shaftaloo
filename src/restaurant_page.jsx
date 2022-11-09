28342d7b346f8a855a0150eed392cb5c41ece0d9
Mafhoot <Mafhoot@gmail.com>
2022-11-09 23:22:42 +0330

    Adding first menu card

--- src/restaurant_page.jsx
+++ src/restaurant_page.jsx
@@ -1,11 +1,15 @@
 import React,{useState} from "react";
-//import pic from "./Images/store-banner.jpg"
+//import pic from "./Images/burger.jpg"
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 export const Restaurant_page = () => {
   const [value, setValue] = React.useState(3.5);
@@ -19,27 +23,47 @@ export const Restaurant_page = () => {
     },
   });
 
-      return (
-        <>
-        <ThemeProvider theme={theme}>
-        <div className="All">
-        <div className="HeadImage"></div>
-        <div className="logo"></div>
-        <div className="details">
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+    <div className="All">
+      <div className="HeadImage"></div>
+      <div className="logo"></div>
+      <div className="details">
         <div className="info" >
-            <label className="name">KFC</label>
-            <p className="description" >The first and best brand of fried foods in the world</p>
-            
-            <p className="Tags">Tags : <ButtonGroup color="neutral" variant="text" aria-label="text button group">
+          <label className="name">KFC</label>
+          <p className="description" >The first and best brand of fried foods in the world</p>
+          <p className="location">Location : North california</p>
+          <p className="Tags">Tags : <ButtonGroup color="neutral" variant="text" aria-label="text button group">
             {tags.map(tags => (
               <Button>{tags}</Button>
             ))}
-            </ButtonGroup></p>
-            <span className="Rate">Rating :</span> <Rating className="rating" name="half-rating-read" defaultValue={value} precision={0.5} readOnly  />
-          </div>
+            </ButtonGroup>
+          </p>
+          <span className="Rate">Rating :</span> <Rating className="rating" name="half-rating-read" defaultValue={value} precision={0.5} readOnly  />
         </div>
+
+        <div className="menu">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia component="img" height="140" image="burger.jpg" alt="green iguana"/>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Burger
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                100gr 80% meat, bread, pickle, tomato
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Place Order</Button>
+            </CardActions>
+          </Card>
         </div>
-        </ThemeProvider>
-        </>
-      )
+
+
+      </div>
+    </div>
+    </ThemeProvider>
+    </>
+  )
 }
