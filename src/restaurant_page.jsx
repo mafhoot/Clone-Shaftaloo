a1faf49f94e62419945074119dfc9b104ac9f074
Mafhoot <Mafhoot@gmail.com>
2022-11-11 00:33:19 +0330

    Menu mapping

--- src/restaurant_page.jsx
+++ src/restaurant_page.jsx
@@ -3,17 +3,18 @@ import React,{useState} from "react";
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 export const Restaurant_page = () => {
-  const [value, setValue] = React.useState(3.5);
+  const [value, setValue] = useState(3.5);
   const tags = ["fast food" , "fried" , "chicken" ]
+  const [link,setLink] = useState("https://realfood.tesco.com/media/images/Burger-31LGH-a296a356-020c-4969-86e8-d8c26139f83f-0-1400x919.jpg")
+  const foods = ["Burger" , "chicken" , "Hot Dog" ,"Pasta", "Fried Potato"]
 
   const theme = createTheme({
     palette: {
@@ -42,25 +43,26 @@ export const Restaurant_page = () => {
           </p>
           <span className="Rate">Rating :</span> <Rating className="rating" name="half-rating-read" defaultValue={value} precision={0.5} readOnly  />
         </div>
-
-        <div className="menu">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia component="img" height="140" image="burger.jpg" alt="green iguana"/>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Burger
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                100gr 80% meat, bread, pickle, tomato
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Place Order</Button>
-            </CardActions>
-          </Card>
-        </div>
-
-
+      </div>
+      <div className="menu">
+      {foods.map(x => (
+        <Card className="MenuCard" sx={{ maxWidth: 345 }}>
+          <CardMedia component="img" height="140" image={link} alt="green iguana" />
+          <CardContent className="card">
+            <Typography gutterBottom variant="h5" component="div" className="typo">
+              {x}
+            </Typography>
+            <Typography className="typo" variant="body2">
+              Meat, Bread, Pickle, Tomato
+            </Typography>
+          </CardContent>
+          <CardActions className="card">
+            <Button size="small">-</Button>
+            <Button size="small">0</Button>
+            <Button size="small">+</Button>
+          </CardActions>
+        </Card>
+      ))}
       </div>
     </div>
     </ThemeProvider>
