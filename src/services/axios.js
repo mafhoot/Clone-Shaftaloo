6eb715a45da3ad94f71a62472f4a5b90636e5dfe
MSNP1381 <mohamadnematpoor@gmail.com>
2022-11-18 21:20:05 +0330

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -9,6 +9,11 @@ export function getbearer (data){
     return axios.post(url+"api/User/BearerToken",data)
 }
 
+<<<<<<< Updated upstream
 export function getRestaurantCards (){
     return axios.get(url+"api/Restaurant?tag=all&size=10&number=0")
+=======
+export function getRestaurantCards (tag="all"){
+    return axios.get(url+`api/Restaurant?tag=${tag}`)
+>>>>>>> Stashed changes
 }
