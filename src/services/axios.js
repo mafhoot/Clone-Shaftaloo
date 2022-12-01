e547e922f6a91fd8038d479d2d63ff8474be8a28
Mafhoot <Mafhoot@gmail.com>
2022-12-01 14:21:25 +0330

    login & sidebar merged

--- src/services/axios.js
+++ src/services/axios.js
@@ -8,3 +8,17 @@ export function postUser(data) {
 export function getbearer (data){
     return axios.post(url+"api/User/BearerToken",data)
 }
+
+export function getUser (data){
+    return axios.get(url+"api/User/"+data)
+}
+export function G(value){
+    return localStorage.getItem(value);
+}
+export function S(value,key){
+    localStorage.setItem(key,value);
+}
+
+export function getBearerToken(email="w@w.w",password="1234567"){
+return axios.post(url+"api/User/BearerToken",{email:email,password:password})
+}
