6122c46e431d7ed067cdc65cf437095f3feef24c
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2022-12-12 20:14:32 +0330

    update style

---
+++ src/components/aa.js
@@ -0,0 +1,84 @@
+
+import React from 'react';
+import ProfileCard from "./ProfileCard";
+// import FormEdit from "./formEdit";
+import {BrowserRouter  , Link, Route , Switch} from "react-router-dom";
+
+
+export default function aa() {
+  return (
+    <div className="background-abc">
+			<div className="bg-pattern-top-abcd"></div>
+		    {/* <div className="bg-pattern-bottom"></div> */}
+
+
+            
+
+
+			{/* <ul>
+				<li> <a href="/formEdit">Form</a></li>
+				<li> <a href="/ProfileCard">Profile</a></li>
+			</ul> */}
+{/* 
+            <ul>
+				<li> <a href="/formEdit">Form</a></li>
+				<li> <a href="/ProfileCard">Profile</a></li>
+			</ul>
+
+
+			<switch>
+				<Route exact path="/formEdit">
+					<FormEdit/>
+				</Route>
+
+				<Route  path="/ProfileCard">
+					<ProfileCard/>
+				</Route>
+			</switch>  */}
+
+
+			<ProfileCard
+				name="Mahdi Fathian"
+				// age="20"
+				city="Iran"
+				followers="80K"
+				likes="803K"
+				photos="1.4K"
+				abc="Email :"
+				phonee="Phone :"
+				username="Username :"
+				password="Password :"
+
+				
+			></ProfileCard>
+ 			{/* <button type="submit" >Log In</button> */}
+			{/* <form className="login-form">
+//                 <button type="submit" >Log In</button>
+//             </form> */}
+ 	</div>
+  )
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/services/axios.js
@@ -0,0 +1,117 @@
+// import { url } from "./consts";
+// import axios from "axios";
+
+export function putUser(data) {
+    const config = {
+        headers: { Authorization: `Bearer ${G("token")}` }
+    };
+    return axios.put(url+"api/User",data,config)
+}
+
+
+// export function postUser(data) {
+//     const config = {
+//         headers: { Authorization: `Bearer ${G("token")}` }
+//     };
+//     return axios.post(url+"api/User/postUser",data,config)
+// }
+
+
+
+
+// export function getUser(data) {
+//     return axios.get(url+"api/User/GetUserData",data)
+// }
+
+// // export function getbearer (data){
+// //     return axios.post(url+"api/User/BearerToken",data)
+// // }
+
+
+// export function G(key){
+//     return localStorage.getItem(key);
+// }
+// export function S(key,value){
+//     localStorage.setItem(key,value);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { url , deployURL } from "./consts";
+import axios from "axios";
+
+export function postUser(data) {
+    return axios.post(url+"api/User/postUser",data)
+}
+
+export function getbearer (data){
+    return axios.post(url+"api/User/BearerToken",data)
+}
+
+export function getUser (){
+    const config = {
+        headers: { Authorization: `Bearer ${G("token")}` }
+    };
+    return axios.get(url+"api/User/GetUserData",config)
+}
+
+export function G(key){
+    return localStorage.getItem(key);
+}
+export function S(key,value){
+    localStorage.setItem(key,value);
+}
+
+export function getBearerToken(email="w@w.w",password="1234567"){
+    return axios.post(url+"api/User/BearerToken",{email:email,password:password})
+}
+export function getRestaurant (data){
+    return axios.get(url+"api/Restaurant/"+data)
+}
+
+export function getMenu (data){
+    return axios.get(url+"api/Restaurant/GetRestaurantMenu/"+data)
+}
+
+export function getTable (restID , start , end) {
+    return axios.get(url+ "api/ReserveTable/GetActiveReserveTablesByRestaurant?restauranId="+restID+"&from="+start+"&to="+end)
+}
+export function getRestaurantCards (tag, number){
+    return axios.get(url+"api/Restaurant?tag="+tag+"&size=10&number="+number)
+}
+
+
+export function postTable (data) {
+    const config = {
+        headers: { Authorization: `Bearer ${G("token")}` }
+    };
+    return axios.post(url+"api/ReserveTable",data,config)
+    
+}
+
+
+export function postOrder (data) {
+    const config = {
+        headers: { Authorization: `Bearer ${G("token")}` }
+    };
+    return axios.post (url+"api/Order",data,config)
+}
